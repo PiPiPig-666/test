b54092dd8960a5c6e5c1f3152f7b797ed03e755b
IsLiii <2206161022@qq.com>
2023-02-26 15:24:23 +0800

    hot-fix 合并冲突

--- src/main/java/com/cxk/test/TestGit.java
+++ src/main/java/com/cxk/test/TestGit.java
@@ -8,6 +8,7 @@ public class TestGit {
         System.out.println("Git");
         System.out.println("Git test");
         System.out.println("Git hot-fix");
+        System.out.println("Hot-fix 合并冲突");
     }
 
 }
