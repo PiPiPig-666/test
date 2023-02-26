2a36ce9311f96a57f970694a8673226a7b165764
IsLiii <2206161022@qq.com>
2023-02-26 15:20:15 +0800

    hot-fix commit

--- src/main/java/com/cxk/test/TestGit.java
+++ src/main/java/com/cxk/test/TestGit.java
@@ -7,6 +7,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Git");
         System.out.println("Git test");
+        System.out.println("Git hot-fix");
     }
 
 }
