04ed3d6bb7aa8e9215757ecd987e0c52ef2eb18f
IsLiii <2206161022@qq.com>
2023-02-26 14:31:44 +0800

    first commit

---
+++ src/main/java/com/cxk/test/TestGit.java
@@ -0,0 +1,11 @@
+package com.cxk.test;
+
+/**
+ * @author IsLiii    Email:jmucbe@gmail.com
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Git");
+    }
+
+}
